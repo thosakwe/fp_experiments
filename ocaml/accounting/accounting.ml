be3dfce834aeb49d1caccc77d10f77dc6182efd6
Tobe O <thosakwe@gmail.com>
2018-09-09 22:15:20 -0400

    Skip empty accounts

--- ocaml/accounting/accounting.ml
+++ ocaml/accounting/accounting.ml
@@ -1,3 +1,4 @@
+open List
 open String
 
 type account = {name: string; balance: float}
@@ -5,16 +6,26 @@ type account = {name: string; balance: float}
 let emptyAccount () =
     {name = ""; balance=0.0}
 
+let accountIsValid a =
+    a.name != "" && a.balance > 0.0
+
+let accountToString account =
+    "Hey"
+
 (* Parse an account of the format <name>=<balance> *)
 let parseAccount (line) =
     if (String.trim line) = "" then
         emptyAccount ()
     else
         let split = String.split_on_char '=' line in
-        let name = List.nth split 0 in
-        let balanceString = List.nth split 1 in
-        let balance = float_of_string balanceString in
-        {name = name; balance = balance}
+
+        if (List.length split) < 2 then
+            emptyAccount ()
+        else
+            let name = List.nth split 0 in
+            let balanceString = List.nth split 1 in
+            let balance = float_of_string balanceString in
+            {name = name; balance = balance}
 
 (* Parse a list of accounts *)
 let parseAccounts (lines) =
@@ -28,10 +39,14 @@ let rec read_line chan s =
         s
 
 let read_file filename =
-    let chan = open_in filename in
-    read_line chan ""
+    try
+        let chan = open_in filename in
+        read_line chan ""
+    with Sys_error(_) ->
+        ""
 
 let readAccountFile =
     let contents = read_file "accounts.txt" in
-    print_string contents
-    (*parseAccounts (String.split_on_char '\n' contents)*)
+    let allAccounts = parseAccounts (String.split_on_char '\n' contents) in
+    let validAccounts = filter accountIsValid allAccounts in
+    List.iter (fun a -> print_string (accountToString a)) validAccounts
